1abeb2ce6951a177d09fdc1eb44667c62679559e
Behruz <bexruzizzatullayev@gmail.com>
2022-04-08 08:29:53 +0500

    Initial commit

---
+++ src/main/java/com/example/exam_last/ExamLastApplication.java
@@ -0,0 +1,13 @@
+package com.example.exam_last;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamLastApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamLastApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/exam_last/ExamLastApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exam_last;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamLastApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
