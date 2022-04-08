b894084bdf27bea2a1ef5a5640f9915c167f19f4
Behruz <bexruzizzatullayev@gmail.com>
2022-04-08 12:15:55 +0500

    zo'r

---
+++ src/main/java/com/example/exam_last/security/CurrentUser.java
@@ -0,0 +1,13 @@
+package com.example.exam_last.security;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+
+import java.lang.annotation.*;
+
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE, ElementType.PARAMETER})
+@AuthenticationPrincipal
+public @interface CurrentUser {
+
+}
